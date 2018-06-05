12d68c19f0e79f301fd3fe3e566f2ed5020aa812
Horacio Brizuela <horaciob@lagash.com>
2018-06-05 00:01:05 -0300

    add an example mongoDB test from Application Console in NetCore

---
+++ Lagash.NoSql.MongoDB/Lagash.NoSql.MongoDB/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lagash.NoSql.MongoDB.Repository.MongoDB;
+using Lagash.NoSql.MongoDB.Models;
+
+namespace Lagash.NoSql.MongoDB
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            IMongoDB service = new MongoDb();
+
+            var customerRepository = new CustomersRepository(service,"customers");
+
+            customerRepository.CreateOrUpdate(new Customer()
+            {
+                Name = "Horacio",
+            });
+        }
+    }
+}
